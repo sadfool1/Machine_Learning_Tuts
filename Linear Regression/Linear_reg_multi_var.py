8110b8e49b6b45f6289fa94dbc28dc63b1bffa2b
James <takw14@gmail.com>
2020-05-21 16:50:21 +0800

    Notes.

--- Linear Regression/Linear_reg_multi_var.py
+++ Linear Regression/Linear_reg_multi_var.py
@@ -24,6 +24,7 @@ we have the slopes known as coefficients and b is known as intercept
 df = pd.read_csv("Book3 - Multiple variables.csv")
 
 median_bedrooms = math.floor(df.Bedrooms.median())
+
 df.Bedrooms = df.Bedrooms.fillna(median_bedrooms) #fills the NaN --> DATA CLEANING
 
 regression = linear_model.LinearRegression()
@@ -31,4 +32,4 @@ regression.fit(df[["Area", "Bedrooms","Age"]],df.Prices)
 
 print (regression.predict([[100000,20,100]]))
 
-
+#Notes: Linear Regression with multi var introduces extra features in a similar way as single Var
